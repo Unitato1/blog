d2e78e3846b4b650ce3e2a2012058ed54d447180
Unitato1 <robertsvihla@gmail.com.github.com>
2024-02-08 11:36:51 +0100

    Added link to new page and error msgs

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 # rails generate model Article title:string body:text
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+
 end
