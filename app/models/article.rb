1963a258542beceb6ca50a05bb7d5b62581fe67b
Unitato1 <robertsvihla@gmail.com.github.com>
2024-02-08 16:49:03 +0100

    added new Comment model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 # rails generate model Article title:string body:text
 class Article < ApplicationRecord
+  has_many :comments
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
